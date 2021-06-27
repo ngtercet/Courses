5e0f8fa3d1c46349feafc5876fc5003564ad3bbb
Jaron <ngtercet@protonmail.com>
2021-06-27 23:48:16 +0800

    handle map tasks

--- 6.824-2021/src/mr/worker.go
+++ 6.824-2021/src/mr/worker.go
@@ -3,8 +3,11 @@ package mr
 import (
 	"fmt"
 	"hash/fnv"
+	"io/ioutil"
 	"log"
 	"net/rpc"
+	"os"
+	"sort"
 )
 
 //
@@ -15,7 +18,16 @@ type KeyValue struct {
 	Value string
 }
 
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 var workerId int64 = -1
+var taskID int = -1
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -34,13 +46,75 @@ func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
-	req := &Req{
-		WorkerId: workerId,
+	for {
+		req := &Req{
+			TaskID:   taskID,
+			WorkerId: workerId,
+		}
+		rsp := &Rsp{}
+		call("Coordinator.AskForTask", req, rsp)
+		type_ := rsp.Type
+		filenamePrefix := rsp.FileNamePrefix
+		nReduce := rsp.NReduce
+		taskID = rsp.TaskID
+		if workerId == -1 {
+			workerId = rsp.WorkerId
+		}
+		switch type_ {
+		case 0:
+			reduceFileMap, err := createReduceFile(taskID, nReduce)
+			if err != nil {
+				log.Fatalf("create reduce files failed, err: %v", err)
+				return
+			}
+			file, err := os.Open(filenamePrefix)
+			if err != nil {
+				log.Fatalf("open source file %v failed", filenamePrefix)
+			} else {
+				if bytes, err := ioutil.ReadAll(file); err != nil {
+					log.Fatalf("cannot read %v", file)
+				} else {
+					kvs := mapf(filenamePrefix, string(bytes))
+					sort.Sort(ByKey(kvs))
+					for _, kv := range kvs {
+						key := kv.Key
+						val := kv.Value
+						idx := ihash(key) % nReduce
+						interFilename := fmt.Sprintf("mr-%d-%d-%d", taskID, idx, workerId)
+						fmt.Fprintf(reduceFileMap[interFilename], "%v %v\n", key, val)
+					}
+				}
+			}
+			file.Close()
+			closeReduceFile(reduceFileMap)
+		case 1:
+		default:
+		}
+		fmt.Printf("%v\n", rsp)
 	}
-	rsp := &Rsp{}
-	call("Coordinator.AskForTask", req, rsp)
-	fmt.Printf("%v", rsp)
 
+}
+
+func createReduceFile(taskID int, nReduce int) (map[string]*os.File, error) {
+	m := make(map[string]*os.File)
+	for i := 0; i < nReduce; i++ {
+		filename := fmt.Sprintf("mr-%d-%d-%d", taskID, i, workerId)
+		file, err := os.Create(filename)
+		if err != nil {
+			return nil, err
+		}
+		m[filename] = file
+	}
+	return m, nil
+}
+
+func closeReduceFile(files map[string]*os.File) error {
+	for _, v := range files {
+		if err := v.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 //
