bbbff911d1bdc1a61617069cb871cb24fb1c4d07
Jaron <ngtercet@protonmail.com>
2021-06-27 18:03:36 +0800

    add worker

---
+++ 6.824-2021/src/mr/worker.go
@@ -0,0 +1,67 @@
+package mr
+
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+)
+
+//
+// Map functions return a slice of KeyValue.
+//
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+var workerId int64 = -1
+
+//
+// use ihash(key) % NReduce to choose the reduce
+// task number for each KeyValue emitted by Map.
+//
+func ihash(key string) int {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
+}
+
+//
+// main/mrworker.go calls this function.
+//
+func Worker(mapf func(string, string) []KeyValue,
+	reducef func(string, []string) string) {
+
+	// Your worker implementation here.
+	req := &Req{
+		WorkerId: workerId,
+	}
+	rsp := &Rsp{}
+	call("Coordinator.AskForTask", req, rsp)
+	fmt.Printf("%v", rsp)
+
+}
+
+//
+// send an RPC request to the coordinator, wait for the response.
+// usually returns true.
+// returns false if something goes wrong.
+//
+func call(rpcname string, args interface{}, reply interface{}) bool {
+	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := coordinatorSock()
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	defer c.Close()
+
+	err = c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
+}
